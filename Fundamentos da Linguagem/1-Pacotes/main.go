c1b5e2fff712c22b6ef83bc6e9f3ce99bb9eb583
Joao Balhego] <jvbbalhego@gmail.com>
2026-07-14 20:59:26 -0300

    Estrutura incial e primeiro pacote auxiliar

---
+++ Fundamentos da Linguagem/1-Pacotes/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"modulo/Auxiliar"
+)
+
+// Função Escrever registra uma mensagem no CLI
+func main() {
+	fmt.Println("Escrevendo do arquivo main")
+	auxiliar.Escrever()
+
+}
