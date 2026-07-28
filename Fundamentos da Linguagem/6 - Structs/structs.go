e5f0734dc4bcc1d8bbdef8a3900ae23dfd2a3721
Joao Balhego] <jvbbalhego@gmail.com>
2026-07-28 20:43:16 -0300

    conceito e estrutura basica de structs

---
+++ Fundamentos da Linguagem/6 - Structs/structs.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+//Tipo com  de variaveis de tipos, parecido com um exemplo com um objeto em POO
+type usuario struct {
+	nome string
+	idade int8
+	endereço endereco
+}
+
+//Podemos usar uma struct dentro de outra struct, como por exemplo a struct endereço abaixo sendo usada como um campo na struct usuário acima
+type endereco struct{
+	rua string
+	numero int16
+	complemento string
+}
+
+func main(){
+
+	var usuario1 usuario
+
+	//Podemos atrubuir um campo e valor por vez
+	usuario1.idade = 18
+	usuario1.nome = "João Victor Balhego"
+	fmt.Println(usuario1)
+
+	//Podemos atrubuir todos os valores da struct de uma vez só, seguindo a ordem de declaração das varaiveis
+	usuario2 := usuario{"Fulano", 21, endereco{"Ruinha da silva", 200, "Centro Historico"}}
+	fmt.Println(usuario2)
+
+	//Podemos atrubuir somente um valor na instancia de uma variavel da struct
+	usuario3 := usuario{nome: "Ciclano"}
+	fmt.Println(usuario3)
+
+
+}
